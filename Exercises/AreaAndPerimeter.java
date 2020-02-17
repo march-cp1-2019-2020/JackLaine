a0803026f3cde4c1fa46be3e8aa551d2ea36e0e7
nortoha <39999692+nortoha@users.noreply.github.com>
2020-02-16 21:09:35 -0600

    Adding Teacher Notes
    Please read through my comments in your code prefixed with "TEACHER NOTES:"

--- Exercises/AreaAndPerimeter.java
+++ Exercises/AreaAndPerimeter.java
@@ -1,11 +1,14 @@
 package ch03_ex2_AreaAndPerimeter;
 
+// TEACHER NOTES: make sure you name the class what the assignment states.  See step 1.
 public class AreaAndPerimeter {
 	// author: jack laine
 	public static void main(String[] args) {
 
 		AreaPerimeter AP = new AreaPerimeter(System.in);
 		System.out.println("welcome to the area and perimeter calculator");
+		
+		//TEACHER NOTES: you are supposed to ask the user to enter length and width. see step 3
 		System.out.println("enter perimeter 1/area 1:");
 		String areaperimeter1 = AP.nextLine();
 		System.out.println("enter area2 / perimeter 2:");
