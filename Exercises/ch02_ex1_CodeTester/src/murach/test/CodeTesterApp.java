83f3c58d4e5d7a1766fa071c6d54f3cccc003476
JackLaine04 <58185814+JackLaine04@users.noreply.github.com>
2020-03-25 12:33:48 -0500

    Add files via upload

--- Exercises/ch02_ex1_CodeTester/src/murach/test/CodeTesterApp.java
+++ Exercises/ch02_ex1_CodeTester/src/murach/test/CodeTesterApp.java
@@ -1,21 +1,30 @@
 package murach.test;
 
+import java.util.Scanner;
+
 public class CodeTesterApp {
 
+
+	
+	
     public static void main(String args[]) {
+    
+    	 Scanner sc = new Scanner(System.in);
+    	 System.out.println("enter name:");
+    	 String name = sc.nextLine();
         System.out.println("Welcome to the Code Tester");
         System.out.println();
-
         String productCode = "java";
         int tax = (int) 2;
         double price = 49.50;
         int quantity = 2;
         double total = price * quantity;
         String message =
+        	"Name:	  "  + name + "\n"+	
             "Code:    " + productCode+ "\n" + 
             "Price:   " + price * 2 + "\n" +
             "Quantity:" + quantity + "\n" +
-            "Tax:     " + tax + "percent" + "\n" +
+            "Tax:     " + tax + " percent" + "\n" +
             "Total:   " + total + "+" + 1.98 + "\n";
         System.out.println(message); 
 
