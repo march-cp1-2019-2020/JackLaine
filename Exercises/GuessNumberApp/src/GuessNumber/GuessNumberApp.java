649866319b8b9dd9f6baae5ec520e7e83fb96c46
nortoha <39999692+nortoha@users.noreply.github.com>
2020-04-03 09:50:48 -0500

    TEACHER NOTES in code
    Looks like you missed adding the while loop on step 5.  Adding the loop will let the user guess multiple times until they get it right.
    Look at your combination of if statements: the way you have them nested will not work if they users first guess is the correct number or is less than randomNumber.  Try this...temporarily change line 14 to set randomNumber to 5.  That way you can predict the random number.  Now run the code...guess 5 and see what happens.

--- Exercises/GuessNumberApp/src/GuessNumber/GuessNumberApp.java
+++ Exercises/GuessNumberApp/src/GuessNumber/GuessNumberApp.java
@@ -13,9 +13,14 @@ public class GuessNumberApp {
 		
 		int randomNumber = (int)(Math.random() * 10);
 		
+		//TEACHER NOTES: missing while loop
 		System.out.println("Please pick a number between 0 and 10");
 		guess = Integer.parseInt(sc.nextLine());
 		
+		/*TEACHER NOTES: the way you have them nested will not work if they users first guess 
+		 * is the correct number or is less than randomNumber.  Try this...temporarily change 
+		 * line 14 to set randomNumber to 5.  That way you can predict the random number.  
+		 * Now run the code...guess 5 and see what happens. */
 		if(guess > randomNumber) {
 			System.out.println("that number is too large!");
 		
