572acf6812706cd2a8a3f92824b3396995a24de1
JackLaine04 <58185814+JackLaine04@users.noreply.github.com>
2020-04-16 15:41:42 -0500

    Add files via upload

---
+++ ch17 ex1 CustomerManager.zip_expanded/ch17_ex1_CustomerManager/src/murach/io/CustomerTextFile.java
@@ -0,0 +1,95 @@
+package murach.io;
+
+
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import murach.business.Customer;
+//Jack Laine
+public class CustomerTextFile {
+    private static final String FIELD_SEP = "\t";
+    private static final Path customersPath = Paths.get("products.txt");     
+    private static final File customersFile = customersPath.toFile();      
+    private static ArrayList<Customer> customers = null; 
+    
+    // prevent instantiation of the class
+    private CustomerTextFile() {}
+
+    public static List<Customer> getCustomers() {
+       try (PrintWriter out = new PrintWriter(
+    		   				  new BufferedWriter
+    		   				  new FileWriter(customers.txt)))){
+    	for (customers c : customers) {
+    		out.print(c.getCode() + FIELD_SEP);
+    		out.print(c.getDescription() + FIELD_SEP);
+    		out.println(c.getPrice());
+    	}
+    		   				  }
+    	if (customers != null)
+            return customers;        
+
+        customers = new ArrayList<>();  
+        String line = in.readLine();
+        while(line !=null) {
+        	String[] columns = line.split(FIELD_SEP);
+        	String code = columns[0];
+        	String description = columns[1];
+        	String price = columns[2];
+        	
+        	Customer c = new Customer();
+        	c.setCode(code);
+        	c.setDescription(description);
+        	c.setPrice(Double.parseDouble(price));
+        	Customer.add(c);
+        }
+        
+        
+
+
+        return customers;            
+    }
+
+    public static Customer getCustomer(String email) {
+        for (Customer c : customers) {
+            if (c.getEmail().equals(email)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public static boolean addCustomer(Customer c) {
+        customers.add(c);
+        return saveCustomers();
+    }
+
+    public static boolean deleteCustomer(Customer c) {
+        customers.remove(c);
+        return saveCustomers();
+    }
+
+    public static boolean updateCustomer(Customer newCustomer) {
+        // get the old customer and remove it
+        Customer oldCustomer = getCustomer(newCustomer.getEmail());
+        int i = customers.indexOf(oldCustomer);
+        customers.remove(i);
+
+        // add the updated customer
+        customers.add(i, newCustomer);
+
+        return saveCustomers();
+    }
+
+    private static boolean saveCustomers() {
+    	fileWriter(customers.txt){
+    		
+    	out.print(get.customers); 
+        return true;
+        try {
+    } catch (IOException e) {
+    System.out.println("error occured");
+        		
+    }
+}
+}
+}
